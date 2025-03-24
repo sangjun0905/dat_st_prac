1e1d564b2ab49387298bf80d0ce0a4d97c2673c8
sangjun0905 <102857053+sangjun0905@users.noreply.github.com>
2025-03-24 05:40:15 +0000

    tlqkf@@22

--- prac1/20221175_prog1_3.c
+++ prac1/20221175_prog1_3.c
@@ -2,34 +2,32 @@
 #include <stdlib.h>
 
 int main() {
-    FILE *file = fopen("maze1-1.txt", "r");
-    if (file == NULL) {
-        perror("파일 열기 실패");
-        return 1;
-    }
+    FILE *fp = fopen("maze1-1.txt", "r");
 
-    int rows, cols;
-    fscanf(file, "%d %d\n", &rows, &cols);  // 첫 줄: 규격 정보
+    int totalRow, totalCol; 
+    fscanf(fp, "%d %d\n", &totalCol, &totalRow);  
+    
 
-    // 미로 저장용 이차원 배열 (문자열 저장)
-    char **maze = malloc(sizeof(char*) * rows);
-    for (int i = 0; i < rows; i++) {
-        maze[i] = malloc(sizeof(char) * (cols + 2)); // '\n' + '\0' 대비
-        fgets(maze[i], cols + 2, file); // 한 줄씩 읽기
+    char **miro = malloc(sizeof(char*) * (2*totalRow));
+    for (int i = 0; i < 2*totalRow; i++) {
+        miro[i] = malloc(sizeof(char) * (totalCol + 1));
+        fgets(miro[i], totalCol + 2, fp); 
     }
 
     // 출력 테스트
-    printf("미로 크기: %d행 %d열\n", rows, cols);
-    for (int i = 0; i < rows; i++) {
-        printf("%s", maze[i]);
+    printf("미로 크기: %d행 %d열\n", totalRow, totalCol);
+    for (int i = 0; i < 2*totalRow; i++) {
+        printf("%s", miro[i]);
     }
 
     // 메모리 해제
-    for (int i = 0; i < rows; i++) {
-        free(maze[i]);
+    for (int i = 0; i < 2*totalRow; i++) {
+        free(miro[i]);
     }
-    free(maze);
+    free(miro);
 
-    fclose(file);
+    fclose(fp);
+
+    ("%d", totalRow);
     return 0;
 }
